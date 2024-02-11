f8437ac3f402eba3f079153f922ed4277a3fd8af
MacWeb <mac.web.company@gmail.com>
2024-02-10 22:46:02 -0500

    MacLearn Anniversary Update 🥳!

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
   const wrap = document.getElementById("wrap");
   const bar = document.getElementById("sidebar");
+  const close = document.getElementById("closea");
+  close.addEventListener("click", function () {
+    close.parentElement.parentElement.style.display = "none";
+    localStorage.setItem("maclearnclose","closed");
+  })
   document.addEventListener("mousemove", function (event) {
     if (event.clientX <= 5 && bar.classList.contains("movingbar") !== true) {
       bar.classList.add("movingbar");
